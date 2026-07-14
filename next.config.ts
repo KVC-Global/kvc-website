0f7f61c6d6e16ed6ca603eab6420b0ed3f534358
HuyDiCode <nbquochuy.forwork@gmail.com>
2026-07-14 23:13:12 +0700

    chore: add images.pexels.com to remotePatterns in nextconfig

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,14 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/**",
+      },
     ],
   },
 }
 
 export default nextConfig
+
