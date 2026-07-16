236d220c716d56eaa806da29e4f9121d907fd54b
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2026-07-16 12:15:33 +0700

    feat: work pass process section

--- components/work-pass/work-pass-details.tsx
+++ components/work-pass/work-pass-details.tsx
@@ -2,12 +2,14 @@
 
 import { Container } from "@/components/ui/container"
 import { WorkPassTarget } from "./work-pass-target"
+import { WorkPassProcess } from "./work-pass-process"
 
 export function WorkPassDetails({ className }: { className?: string }) {
   return (
     <div className="w-full bg-brand-light py-16 md:py-24">
       <Container className={className}>
         <WorkPassTarget />
+        <WorkPassProcess />
       </Container>
     </div>
   )
