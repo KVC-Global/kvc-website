5bf7814a085ab3ea17ea13d23dd6a8143662165d
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2026-07-18 09:08:18 +0700

    feat: fix ci pipeline

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity"
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "eh8b0fvx",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2026-07-18",
   useCdn: true, // Use CDN for client-side fetches; defineLive will configure server-side fetches.
 })
